ac8fcd6a74e6a82d7a409c88e9b3b73c4cce66fa
June-24 <mj21csb0b36@student.nitw.ac.in>
2023-08-31 13:24:37 +0530

    yes

--- Chat Server using Messagequeues/client.cpp
+++ Chat Server using Messagequeues/client.cpp
@@ -1,48 +1,59 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
+#include <bits/stdc++.h>
 #include <sys/msg.h>
-#include <string.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <stdio.h>
 #include <unistd.h>
-#include<sys/poll.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <fstream>
+#include <sys/wait.h>
+#include <poll.h>
+using namespace std;
 
-struct my_buffer {
-    int type;
-    char data[100];
-    int pid; // Client's PID
-} message;
+struct my_buffer 
+{
+    long type;
+    char data[512];
+};
 
-int main() {
-    key_t key_server = ftok("./server_queue.txt", 65);
-    int msqid_server = msgget(key_server, 0666 | IPC_CREAT);
+int main ()
+{
+    // creating the msg queues
+    int key_server = ftok("./server.txt" , 65);
+    int key_client = ftok("./client.txt" , 65);
+    int msqid_server = msgget(key_server , IPC_CREAT | 0666);
+    int msqid_client = msgget(key_client , IPC_CREAT | 0666);
 
-    key_t key_client = ftok("./client_queue.txt", 1);
-    int msqid_client = msgget(key_client, 0666 | IPC_CREAT);
-    // creating the pollfds 
-    struct pollfd pfd[2];
-    int users = 0;
+    // sending teh pid first to server
+    long pid = (long)getpid();
+    struct my_buffer sending_pid;
+    sending_pid.type = 1;//first time is 1
+    char const* pidd = (to_string(getpid())).c_str();
+    strncpy(sending_pid.data,pidd,strlen(pidd));
+    sending_pid.data[strlen(pidd)] = '\0';
+    msgsnd(msqid_server , &sending_pid , strlen(sending_pid.data) , 0);// sending the pid to server
 
-    // initialising the pollfds
-    pfd[0].fd = 0; pfd[0].events = POLLIN; 
-    pfd[1].fd = msqid_client; pfd[1].events = POLLIN;
-    while (1) {
-        // Polling
-        int ret = poll(pfd, 2, 10);
-        if (pfd[0].revents & POLLIN) {
-            // Read from stdin
-            fgets(message.data, sizeof(message.data), stdin);
-            message.type = 1; // Indicate it's a client message
-            message.pid = getpid();
-            
-            // Send message to server
-            msgsnd(msqid_server, &message, sizeof(message), 0);
+    // polling the keyboard
+    struct pollfd pfd[1];
+    pfd[0].fd = 0; pfd[0].events = POLLIN;
+    while (1)
+    {
+        int s = poll(pfd , 1 , 10);
+        if (s>0 && pfd[0].revents & POLLIN)
+        {
+            struct my_buffer temp;
+            fgets(temp.data , 10 , stdin);
+            temp.type = pid;
+            msgsnd(msqid_server , &temp , strlen(temp.data) , 0);
         }
-        if (pfd[1].revents & POLLIN) {
-            // Receive response from server
-            msgrcv(msqid_client, &message, sizeof(message), 0, 0);
-            printf("Received from Server: %s\n", message.data);
+
+        struct my_buffer temp1;
+        int t = msgrcv(msqid_client , &temp1 , sizeof(temp1.data) , getpid() , MSG_NOERROR | IPC_NOWAIT);
+        
+        if (t!=-1){
+            temp1.data[t] = '\0';
+            cout<<"User #"<<temp1.type<<" : "<<temp1.data<<endl;
         }
     }
-
-    return 0;
 }
--- Chat Server using Messagequeues/server.cpp
+++ Chat Server using Messagequeues/server.cpp
@@ -1,43 +1,55 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
+#include <bits/stdc++.h>
 #include <sys/msg.h>
-#include <string.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <fstream>
+#include <sys/wait.h>
+#include <poll.h>
+using namespace std;
+struct my_buffer 
+{
+    long msgtype;
+    char msgtext[512];
+};
+int main ()
+{
+    // creating the msg queues
+    int key_server = ftok("./server.txt" , 65);
+    int key_client = ftok("./client.txt" , 65);
+    int msqid_server = msgget(key_server , IPC_CREAT | 0666);
+    int msqid_client = msgget(key_client , IPC_CREAT | 0666);
+    map<int,int> mp;
+    while (1)
+    {   
+        struct my_buffer temp;
+        int t = msgrcv(msqid_server , &temp , sizeof(temp.msgtext) , 0 , 0);
+        if (t!=-1)
+        {
+            // for first time, inserting into map
+            if (temp.msgtype==1)
+            {   
+                cout<<"User#"<<temp.msgtext<<" joined the chat "<<endl;
+                mp[stoi(temp.msgtext)] = 1;
+                continue ;
+            }
+            // sending to all who are didnt send the message
+            for (auto j : mp)
+            {
+                if (j.first!=temp.msgtype)
+                {
+                    struct my_buffer temp1;
+                    temp1.msgtype = j.first;
+                    strncpy(temp1.msgtext, temp.msgtext, sizeof(temp1.msgtext) - 1);
+                    temp1.msgtext[sizeof(temp1.msgtext) - 1] = '\0';
 
-struct my_buffer {
-    int type;
-    char data[100];
-    int pid; // Server's PID
-} message;
-
-int main() {
-    key_t key_server = ftok("./server_queue.txt", 65);
-    int msqid_server = msgget(key_server, 0666 | IPC_CREAT);
-
-    // Use an array of message queues for multiple clients
-    int msqid_clients[10]; // Assuming a maximum of 10 clients
-
-    for (int i = 0; i < 2; ++i) {
-        key_t key_client = ftok("./client_queue.txt", i + 1);
-        msqid_clients[i] = msgget(key_client, 0666 | IPC_CREAT);
-    }
-
-    while (1) {
-        msgrcv(msqid_server, &message, sizeof(message), 0, 0);
-        
-        if (message.type == 1) {
-            // Received a client message
-            printf("Received from Client [%d]: %s\n", message.pid, message.data);
-
-            // Broadcast received message to all connected clients
-            for (int i = 0; i < 2; ++i) {
-                if (msqid_clients[i] != -1) {
-                    msgsnd(msqid_clients[i], &message, sizeof(message), 0);
+                    int isfine = msgsnd(msqid_client, &temp1, strlen(temp1.msgtext) + 1, 0);
+                    cout<<"Sent message .."<<endl;
                 }
             }
         }
     }
-
-    return 0;
 }
